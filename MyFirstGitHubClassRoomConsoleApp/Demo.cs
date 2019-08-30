8b47242b35fc8241aae17a75504f1551d46cdc17
Popular Belbase <popu0003@edu.easj.dk>
2019-08-30 14:42:33 +0200

    add class

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    public class Demo
+    {
+        private string _name;
+        private int _age;
+        private string _address;
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        public Demo(string name, int age, string address)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+    }
+}
